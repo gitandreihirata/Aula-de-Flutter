b6a3e05f567422fa72b6c22246e845145da643fc
gitandreihirata <161364775+gitandreihirata@users.noreply.github.com>
2025-08-13 16:43:31 -0300

    Apostila 1 e 2
    Apostila 1 e 2

--- FlutterCodes/funcoes.dart
+++ FlutterCodes/funcoes.dart
@@ -1,9 +1,19 @@
 // Esta função não retorna valor (void) e não recebe parâmetros ().
 void main() {
-  print('O aplicativo começou aqui!');
-}
+  print('Funcoes');
+
 
 // Esta função recebe um nome (String) e retorna uma saudação (String).
 String saudar(String nome) {
   return 'Olá, $nome! Bem-vindo(a) ao Flutter.';
+}
+
+String Mensagem(String msg){
+    return 'A sua mensagem é $msg';
+}
+  
+print(saudar("Andre"));
+print(Mensagem('Era uma vez'));
+
+
 }
---
+++ FlutterCodes/list.dart
@@ -0,0 +1,19 @@
+void main() {
+ 
+  //Lista de informações - linguagens
+  List<String> linguagens = ['Dart','C++','JavaScript'];
+  
+  //Valores da lista
+  print(linguagens);
+  
+  //Adiciona valores na lista
+  
+  linguagens.add('Java');
+  print(linguagens);
+  
+  //Posicao do valor
+  print(linguagens[2]);
+
+  
+  
+}
